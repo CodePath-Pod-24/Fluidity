7f1770bc5609e31bb8ef38bbf3254abbec4fd5fd
sherrss <liu1500sherry@gmail.com>
2022-11-11 00:46:16 -0500

    Update files

---
+++ Fluidity/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  Fluidity
+//
+//  Created by Sherry Liu on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 240, height: 128))
+        imageView.image = UIImage(named: "logo-no-background")
+        return imageView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(imageView)
+
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.center = view.center
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+            self.animate()
+        })
+    }
+
+    private func animate() {
+        UIView.animate(withDuration: 1, animations: {
+            let size = self.view.frame.size.width * 3
+            let diffX = size - self.view.frame.size.width
+            let diffY = self.view.frame.size.height - size
+            
+            self.imageView.frame = CGRect(x: -(diffX/2),
+                                          y: diffY/2,
+                                          width: size,
+                                          height: size
+            )
+            
+        })
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            self.imageView.alpha = 0
+        }, completion: { done in
+            if done {
+                let viewController = LoginViewController()
+                viewController.modalTransitionStyle = .crossDissolve
+                viewController.modalPresentationStyle = .fullScreen
+                self.present(viewController, animated: true)
+            }
+        })
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
